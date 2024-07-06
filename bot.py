9665aebb3cacb50e46a7dd145041d4912dfdfcd1
Roman Krupkin <krups1220@gmail.com>
2024-07-06 13:47:59 +0300

    Начальный шаблон проекта

---
+++ bot.py
@@ -0,0 +1,59 @@
+import asyncio
+import logging
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats
+from aiogram.filters.command import Command
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config["TG_TOKEN"])
+# Диспетчер
+dp = Dispatcher()
+
+
+
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Создать турнир"),
+            types.KeyboardButton(text="Помощь")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        
+    )
+    await message.answer("Привет, ты зашел в бота Rctournaments4all, что бы ты хотел сделать?", reply_markup=keyboard)
+
+
+
+
+@dp.message(F.text=="Помощь")
+async def start(message: types.Message):
+    await message.answer("Ты молодец, у тебя все получится!")
+
+@dp.message(F.text=="Создать турнир")
+async def start(message: types.Message):
+    await message.answer("Введите имена пилотов одним сообщением.")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
+    
